4ff284d91e0a4c7598ffd1c6fa3794f08e1cecd7
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2024-04-25 15:09:50 +1000

    Add more tests cases for path graphs

--- test_main.py
+++ test_main.py
@@ -7,8 +7,12 @@ from main import GameState, minimax
 test_cases = [
     # Path graph with 3 vertices where cop and robber start on opposite ends.
     ([(0, 1), (1, 2)], 0, 2, 1),
+    # Path graph with 3 vertices where cop and robber start adjacent to each other.
+    ([(0, 1), (1, 2)], 0, 1, 0),
     # Path graph with 4 vertices where cop and robber start on opposite ends.
     ([(0, 1), (1, 2), (2, 3)], 0, 3, 1),
+    # Path graph with 4 vertices where robber starts distance two from cop.
+    ([(0, 1), (1, 2), (2, 3)], 0, 2, 2),
     # Path graph with 5 vertices where cop and robber start on opposite ends.
     (
         [(0, 1), (1, 2), (2, 3), (3, 4)],
