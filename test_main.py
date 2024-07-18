e8ff448382dc350416d55813d0be9887889359c8
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2024-07-18 14:05:32 +1000

    Add test cases

--- test_main.py
+++ test_main.py
@@ -9,12 +9,18 @@ test_cases = [
     ([(0, 1), (1, 2)], 0, 2, 1),
     # Path graph with 3 vertices where cop and robber start adjacent to each other.
     ([(0, 1), (1, 2)], 0, 1, 0),
-    # Path graph with 4 vertices where cop and robber start on opposite ends.
-    ([(0, 1), (1, 2), (2, 3)], 0, 3, 1),
+    # Path graph with 4 vertices where robber starts distance zero from cop.
+    ([(0, 1), (1, 2), (2, 3)], 2, 2, 0),
+    # Path graph with 4 vertices where robber starts distance one from cop.
+    ([(0, 1), (1, 2), (2, 3)], 1, 2, 0),
     # Path graph with 4 vertices where robber starts distance two from cop.
     ([(0, 1), (1, 2), (2, 3)], 0, 2, 2),
+    # Path graph with 4 vertices where robber starts distance three from cop.
+    ([(0, 1), (1, 2), (2, 3)], 0, 3, 1),
     # Path graph with 5 vertices where cop and robber start on opposite ends.
     ([(0, 1), (1, 2), (2, 3), (3, 4)], 0, 4, 2),
+    # Path graph with 5 vertices where robber starts distance two from cop.
+    ([(0, 1), (1, 2), (2, 3), (3, 4)], 2, 4, 1),
     # 3-cycle.
     ([(0, 1), (1, 2), (2, 0)], 0, 1, 0),
     # 4-cycle where cop and robber start adjacent to each other.
